dea884c1b5fb145f16daf20f81ee31f49c8313f8
Amit Karn <karnamit3@gmail.com>
2025-09-10 22:45:43 +0530

    Update masterKeyHashIteration value and fix typo in masterKeyDefaultIteration

--- src/config/PasswordManagerConfig.js
+++ src/config/PasswordManagerConfig.js
@@ -25,7 +25,7 @@ const passwordManagerConfig = {
   homePage: "/",
   rahaysa: "रahasya",
   fileEncryptionIterations: 1000,
-  masterKeyHashIteration: 5_00_000,
+  masterKeyHashIteration: 10_000,
   masterKeyDefaultIteraion: 10_000,
   masterKeyIteraionList: [
     1_000, 10_000, 1_00_000, 5_00_000, 1_000_000, 5_000_000, 10_000_000, 20_000_000,
